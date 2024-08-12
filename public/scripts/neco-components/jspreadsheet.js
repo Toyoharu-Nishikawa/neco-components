ce333d7155b58d7132fb1682a8e437a607eddce2
nishikawa <>
2024-08-13 01:42:59 +0900

    added architecture

--- public/scripts/neco-components/jspreadsheet.js
+++ public/scripts/neco-components/jspreadsheet.js
@@ -1,5 +1,5 @@
-await import("./modules/jsuites/dist/jsuites.js")
-await import("./modules/jspreadsheet-ce/dist/index.js")
+import "./modules/jsuites/dist/jsuites.js"
+import "./modules/jspreadsheet-ce/dist/index.js"
 
 const url = import.meta.url
 const cssMainPath = url.split("/").slice(0,-1).join("/")+"/modules/jspreadsheet-ce/dist/jspreadsheet.css"
@@ -8,16 +8,18 @@ const cssThemePath = url.split("/").slice(0,-1).join("/")+"/modules/jspreadsheet
 const cssJsuitesPath = url.split("/").slice(0,-1).join("/")+"/modules/jsuites/dist/jsuites.css"
 
 const tagName = "neco-jspreadsheet"
-
 const template = (params) => `
 <style>
 :host{
-  height: 100%;
+/*  height: 100%;
   width: 100%;
+  */
 }
 #jspreadsheet{
+/*
   height: 100%;
   width: 100%;
+  */
 }
 </style>
 <link rel="stylesheet" href=${cssJsuitesPath}>
@@ -31,8 +33,6 @@ const customElem = class extends HTMLElement {
   constructor(){
     super()
     this.shadow
-    this._contents = this.contents
-    delete this.contents 
   }
   connectedCallback() {
     const defaultData = [["","",""],["","",""],["","",""]]
@@ -41,11 +41,6 @@ const customElem = class extends HTMLElement {
       {type:"text", title:"B"},
       {type:"text", title:"C"},
     ]
-    const params = {
-      data:  this.dataset.data ?? JSON.stringify(defaultData),
-      columns: this.dataset.columns ?? JSON.stringify(defaultColumns),
-      editable:true,
-    }
 
     const shadow = this.attachShadow({mode: 'open'});
     this.shadow=shadow
@@ -56,29 +51,16 @@ const customElem = class extends HTMLElement {
     shadow.appendChild(dom.body.querySelector("div"))
     const divElem = shadow.querySelector("#jspreadsheet")
     this.divElem = divElem
-    //const data = params.data ?  JSON.parse(params.data): null
-    //const colHeaders = params.colHeaders ? JSON.parse(params.colHeaders): true
-//    const rect = shadow.host.getBoundingClientRect()
-//    const tableWidth  = rect.width +"px"
-//    const tableHeight = rect.height + "px"
- 
-//    const data = JSON.parse(params.data)
-//    const columns = JSON.parse(params.columns)
-    const contents = this._contents
-
-    if(contents){
-      this.setContents(contents)
-    }
   }
   get contents(){
     return this._contents
   }
   set contents(contents){
-    this._contents = contents
+    //this._contents = contents
     this.setContents(contents)
   }
   setContents(contents){
-    this.jspreadsheet?.destroy()
+    this.jsp?.destroy()
     const rect = this.divElem.getBoundingClientRect()
     const width  = rect.width
     const height = rect.height
@@ -91,7 +73,12 @@ const customElem = class extends HTMLElement {
     }
     const mergedContents = Object.assign(target, contents)
 
-    const jsp = jspreadsheet(this.divElem, contents) 
+    const jsp = jspreadsheet(this.divElem, mergedContents) 
+    this.jsp=jsp
+    this.shadow.host.onblur=(e)=>{
+        jsp.resetSelection()
+    }
+    
     const resizeObserver = new ResizeObserver((entries) => {
       const e = entries[0]
       const rect = e.target.getBoundingClientRect()
@@ -99,12 +86,20 @@ const customElem = class extends HTMLElement {
       const height = rect.height
       this.resize(width,height)
     })
-    this.jspreadsheet=jsp
-    resizeObserver.observe(this.divElem)
+    resizeObserver.observe(this.shadow.host)
+  }
+  getData(){
+    return this.jsp.getData()
+  }
+  set data(data){
+    this.setData(data)
+  }
+  setData(data){
+    this.jsp.setData(data)
   }
   resize(width,height){
-    this.jspreadsheet.content.style.width = width + "px"
-    this.jspreadsheet.content.style.height = height + "px"
+    this.jsp.content.style.width = width + "px"
+    this.jsp.content.style.height = height + "px"
   }
 
 }
