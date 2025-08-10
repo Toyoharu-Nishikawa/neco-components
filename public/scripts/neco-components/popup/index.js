088e0f42a858ecd14db923eb1cbc689e03c9dde0
nishikawa <>
2025-08-10 18:19:26 +0900

    added function to resize byt the form of nesw

--- public/scripts/neco-components/popup/index.js
+++ public/scripts/neco-components/popup/index.js
@@ -100,7 +100,7 @@ const createHTML = (params) => `
   </div>
 </div>
 `
-//
+
 
 let Z_INDEX = 9999
 export const CustomElem = class extends HTMLElement {
@@ -191,8 +191,8 @@ export const CustomElem = class extends HTMLElement {
 
     let mouseIsDownForW = false
     let mouseIsDownForH = false
-    const Dmin = 2 
-    const Dmax = 8
+    const Dmin = 1 
+    const Dmax = 10 
     const mouseDown = (e) => {
       e.stopPropagation()
       e.preventDefault()
@@ -210,7 +210,7 @@ export const CustomElem = class extends HTMLElement {
       if(dw){
         mouseIsDownForW = true
       }
-      else if(dh){
+      if(dh){
         mouseIsDownForH = true
       }
     }
@@ -233,10 +233,13 @@ export const CustomElem = class extends HTMLElement {
       const dy = mainHeight - offsetY
       const dw = Dmin < dx && dx < Dmax 
       const dh = Dmin < dy && dy < Dmax
-      if(dw){
+      if(dw && dh){
+        mainElem.style.cursor = "nwse-resize"
+      }
+      else if(dw && !dh){
         mainElem.style.cursor = "ew-resize"
       }
-      else if(dh){
+      else if(!dw && dh){
         mainElem.style.cursor = "ns-resize"
       }
       else{
@@ -289,7 +292,7 @@ export const CustomElem = class extends HTMLElement {
     this.setQuerySelector()
   }
   resize(width, height){
-     const mainHeight = height - HEADER_HEIGHT
+    const mainHeight = height - HEADER_HEIGHT
     this.childElem.style.width=width+"px"
     this.childElem.style.height=height+"px"
     this.mainElem.style.height=mainHeight+"px"
